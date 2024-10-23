7b958eb49770017e40906737ba265fdbe03105f9
2rigor <39034718+2rigor@users.noreply.github.com>
2024-10-23 16:22:46 +0300

    change make limit constant be variable
    Signed-off-by: 2rigor <39034718+2rigor@users.noreply.github.com>

--- pkg/file/tarutil.go
+++ pkg/file/tarutil.go
@@ -14,7 +14,7 @@ import (
 	"github.com/anchore/stereoscope/internal/log"
 )
 
-const perFileReadLimit = 10 * GB
+var perFileReadLimit = 1 * GB
 
 var ErrTarStopIteration = fmt.Errorf("halt iterating tar")
 
